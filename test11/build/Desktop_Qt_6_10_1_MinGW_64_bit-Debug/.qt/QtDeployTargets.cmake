8cb7964c92571f90e6493c03d5def6a50df11b37
deLong34 <delong@bk.ru>
2026-01-12 16:16:29 +0300

    start

---
+++ test11/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_test11_FILE E:/MAYO/Qt/TestRep/Test11/test11/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/test11.exe)
+set(__QT_DEPLOY_TARGET_test11_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_test11_RUNTIME_DLLS E:/QtCreator/6.10.1/mingw_64/bin/Qt6Widgets.dll;E:/QtCreator/6.10.1/mingw_64/bin/Qt6Gui.dll;E:/QtCreator/6.10.1/mingw_64/bin/Qt6Core.dll)
